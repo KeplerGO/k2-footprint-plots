87f3f15fdadc06e0dfbc543c9f08203daaa47e65
Christina Hedges <clh93@cam.ac.uk>
2017-08-08 14:05:19 -0700

    Updated C0-C3 plots

---
+++ plot-c1.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as pl
+from matplotlib import style
+from matplotlib.ticker import MultipleLocator
+import matplotlib.patheffects as path_effects
+import numpy as np
+from K2fov import plot
+
+from fieldplot import annotate_target
+
+CAMPAIGN = 1
+
+style.use('gray.mplstyle')
+p = plot.K2FootprintPlot(figsize=(11, 11))
+p.plot_campaign(1, annotate_channels=False, facecolor='#aaaaaa', lw=0)
+p.plot_campaign(CAMPAIGN, annotate_channels=False, facecolor='white', lw=1)
+
+
+
+pl.suptitle('K2 Campaign {}'.format(CAMPAIGN), fontsize=44)
+pl.xlim([182, 166])
+pl.ylim([-8, 10])
+p.ax.xaxis.set_major_locator(MultipleLocator(2))
+p.ax.yaxis.set_major_locator(MultipleLocator(2))
+pl.tight_layout()
+
+
+ylim=p.ax.get_ylim()
+xlim=p.ax.get_xlim()
+import pandas as pd
+df=pd.read_csv('catalogs/k2candidates.csv',comment='#')
+for ra,dec,name in zip(df.ra,df.dec,df.pl_name):
+	if (ra>np.min(xlim)) and (ra<=np.max(xlim)) and (dec>np.min(ylim)) and (dec<=np.max(ylim)):
+		annotate_target(ra,dec,name)
+
+
+
+for extension in ['png', 'eps']:
+    pl.savefig('k2-c{}-field.{}'.format(CAMPAIGN, extension), dpi=100)
+pl.close()
+
