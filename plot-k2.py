40af5c5fe35bc94d951c7e7745c73ffffc89b05b
Christina Hedges <christina.l.hedges@nasa.gov>
2018-06-03 14:32:06 -0700

    Better C20

--- plot-k2.py
+++ plot-k2.py
@@ -18,25 +18,25 @@ EXTENSIONS = ['png', 'eps']
 style.use('wendy.mplstyle')
 colors = pl.rcParams["axes.prop_cycle"].by_key()["color"]
 xlims = {
-         0: [107, 89], 1: [182, 166], 2: [256, 237],
-         3: [346, 328], 4: [69, 50.5], 5: [139, 122.5],
-         6: [213, 197], 7: [297, 279], 8: [25, 8],
-         9: [279.5, 262], 10: [194.5, 178.5], 11: [269.5, 251.],
-         12: [360, 343.], 13: [82, 63.5], 14: [168, 153],
-         15: [242.5, 225.5], 16: [142, 125.5], 17: [212.9, 195.5],
-         18: [139, 121.5], 19: [356.5, 339.], 20: [77.5, 58.5],
-         1002: [367, 350.5]
-         }
+    0: [107, 89], 1: [182, 166], 2: [256, 237],
+    3: [346, 328], 4: [69, 50.5], 5: [139, 122.5],
+    6: [213, 197], 7: [297, 279], 8: [25, 8],
+    9: [279.5, 262], 10: [194.5, 178.5], 11: [269.5, 251.],
+    12: [360, 343.], 13: [82, 63.5], 14: [168, 153],
+    15: [242.5, 225.5], 16: [142, 125.5], 17: [212.9, 195.5],
+    18: [139, 121.5], 19: [356.5, 339.], 20: [77.5, 58.5],
+    1002: [367, 350.5]
+}
 ylims = {
-         0: [13.5, 31], 1: [-7.5, 10.5], 2: [-30, -13],
-         3: [-20, -1], 4: [10.5, 28.9], 5: [9.5, 25.9],
-         6: [-19, -2.5], 7: [-31.5, -14.1], 8: [-3, 15],
-         9: [-29.8, -12.5], 10: [-11.8, 5.], 11: [-32, -15],
-         12: [-13.5, 5], 13: [12.5, 29.9], 14: [-1, 15.9],
-         15: [-27.5, -11.5], 16: [11.3, 27], 17: [-16, 1.2],
-         18: [8.5, 26], 19: [-13.5, 5.5], 20: [14.5, 32],
-         1002: [-11, 7.5]
-         }
+    0: [13.5, 31], 1: [-7.5, 10.5], 2: [-30, -13],
+    3: [-20, -1], 4: [10.5, 28.9], 5: [9.5, 25.9],
+    6: [-19, -2.5], 7: [-31.5, -14.1], 8: [-3, 15],
+    9: [-29.8, -12.5], 10: [-11.8, 5.], 11: [-32, -15],
+    12: [-13.5, 5], 13: [12.5, 29.9], 14: [-1, 15.9],
+    15: [-27.5, -11.5], 16: [11.3, 27], 17: [-16, 1.2],
+    18: [8.5, 26], 19: [-13.5, 5.5], 20: [14.5, 32],
+    1002: [-11, 7.5]
+}
 
 cluster_fnames = glob('catalogs/*.csv')
 
@@ -148,16 +148,19 @@ def annotate_extended(CAMPAIGN):
                         marker='d', color=colors[0], zorder=5)
     annotate_target(267.02083, -24.78000, "Terzan 5", marker='d', color=colors[0], zorder=5)
 
-    annotate_target(260.14296371, -19.33374974, "HD 156846 b", color=colors[0], marker='.', zorder=5)
+    annotate_target(260.14296371, -19.33374974, "HD 156846 b",
+                    color=colors[0], marker='.', zorder=5)
     if CAMPAIGN != 9:
         annotate_target(268.118363, -26.703469, "V767 Sgr", color=colors[0], marker='.', zorder=5)
     if CAMPAIGN != 2:
         annotate_target(255.03962888, -24.98907067, "26 Oph", color=colors[0], marker='.', zorder=5)
     annotate_target(258.83743307, -26.60282143, "36 Oph",
                     color=colors[0], marker='.', ha='right', zorder=5)
-    annotate_target(353.616168, -1.580036, "WASP-28 b", color=colors[0], marker='.', zorder=5, ha='right')
+    annotate_target(353.616168, -1.580036, "WASP-28 b",
+                    color=colors[0], marker='.', zorder=5, ha='right')
     if CAMPAIGN != 19:
-        annotate_target(347.29469878, -02.26074375, "HD 218566 b", color=colors[0], marker='.', zorder=5)
+        annotate_target(347.29469878, -02.26074375, "HD 218566 b",
+                        color=colors[0], marker='.', zorder=5)
     annotate_target(346.62233, -5.04144, "TRAPPIST-1", ha='left',
                     color=colors[0], marker='.', zorder=5)
     annotate_target(351.77015194, -1.28627266, "GJ 9827", ha='left',
@@ -167,6 +170,8 @@ def annotate_extended(CAMPAIGN):
                     ha='right', color=colors[0], marker='.', zorder=5)
     annotate_target(201.29824736, -11.16131949, "Spica",
                     ha='left', color=colors[0], marker='.', zorder=5)
+    annotate_target(68.466687, 22.841717, "CL Tau", color=colors[0],
+                    marker='.', zorder=5)
     if CAMPAIGN != 20:
         annotate_target(69.31157942, +18.54303399, "SZ Tau", ha='right',
                         color=colors[0], marker='.', zorder=5)
@@ -201,8 +206,10 @@ def annotate_extended(CAMPAIGN):
     annotate_target(187.27789633, +2.05240633, "3C 273", ha='right', extended=True, zorder=5)
     annotate_target(16.2258, +2.1333, "IC 1613", ha='left', extended=True, zorder=5)
     if CAMPAIGN != 19:
-        annotate_target(353.9553542, 0.4455122, "HIP 116454 b", ha='left', color=colors[0], marker='.', zorder=5)
-    annotate_target(8.739684, 4.381469, "HD 3167 bcd", ha='right', color=colors[0], marker='.', zorder=5)
+        annotate_target(353.9553542, 0.4455122, "HIP 116454 b",
+                        ha='left', color=colors[0], marker='.', zorder=5)
+    annotate_target(8.739684, 4.381469, "HD 3167 bcd", ha='right',
+                    color=colors[0], marker='.', zorder=5)
 
 
 def annotate_microlensing():
@@ -298,8 +305,8 @@ def plot_all():
     _plot(17, planets=False, supernovae=True)
     _plot(18)
     _plot(19)
+    _plot(20)
 
 
 if __name__ == '__main__':
     plot_all()
-    
